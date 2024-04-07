00d98feeae85df9b155e4abe032b65a62698dd02
Serj Phone <serj-by@users.noreply.github.com>
2024-04-07 12:43:38 +0300

    fixed attrs

--- py/shcolar/test.py
+++ py/shcolar/test.py
@@ -7,7 +7,7 @@ def test_pkg(attr_n):
 		if r==-1:
 			print(shcolar.bg.white+shcolar.fg.black,"wbbf")
 		for c in range(shcolar.colors_n):
-			ctrl_vals=str(  30+c)+";"+str(((40+r) if r != -1 else 102))
+			ctrl_vals=str(attr_n)+";"+str(  30+c)+";"+str(((40+r) if r != -1 else 102))
 			vals=" "+str(c)+("-" if r==-1 else str(r))+" "
 			print (shcolar.ctrl_pfx+ctrl_vals+shcolar.ctrl_sfx+vals+shcolar.reset, end=("*" if c<7 else "\n"))
 		if r==-1:
@@ -15,5 +15,5 @@ def test_pkg(attr_n):
 
 if __name__=="__main__":
 	print ("Test Colors tables")
-	for at in range(10):
+	for at in range(1,10):
 		test_pkg(at);
