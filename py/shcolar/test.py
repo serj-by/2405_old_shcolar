d39163e412fe5d6077a769c80b0dc9029e349bee
Serj.by <5691613+serj-by@users.noreply.github.com>
2024-04-07 12:36:36 +0300

    delete comment with aytrs test.py

--- py/shcolar/test.py
+++ py/shcolar/test.py
@@ -7,7 +7,7 @@ def test_pkg(attr_n):
 		if r==-1:
 			print(shcolar.bg.white+shcolar.fg.black,"wbbf")
 		for c in range(shcolar.colors_n):
-			ctrl_vals=/*str(attr_n)+".;"+*/str(  30+c)+";"+str(((40+r) if r != -1 else 102))
+			ctrl_vals=str(  30+c)+";"+str(((40+r) if r != -1 else 102))
 			vals=" "+str(c)+("-" if r==-1 else str(r))+" "
 			print (shcolar.ctrl_pfx+ctrl_vals+shcolar.ctrl_sfx+vals+shcolar.reset, end=("*" if c<7 else "\n"))
 		if r==-1:
