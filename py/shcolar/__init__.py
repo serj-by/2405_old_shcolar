80d0e9e55132e6fbd28674aa364c46e3d5c6620e
Serj Phone <serj-by@users.noreply.github.com>
2024-04-07 10:17:23 +0300

    test addons

--- py/shcolar/__init__.py
+++ py/shcolar/__init__.py
@@ -4,20 +4,21 @@ __author__="Serj.by"
 
 from . import fg
 from . import bg
-__all__=['ctrl_sfx', 'ctrl_pfx', 'reset', 'fg_reset', 'bg_reset', 'fg', 'bg']
+__all__=['esc', 'colots_n', 'ctrl_sfx', 'ctrl_pfx', 'reset', 'fg_reset', 'bg_reset', 'fg', 'bg']
 
 
 reset="\033[0m";
 fg_reset="\033[39m";
 bg_reset="\033[49m";
 
-ctrl_pfx="\033["
+esc="\033";
+ctrl_pfx=esc+"[";
 ctrl_sfx="m"
-
+colors_n=8
 
 def bold(col):
 	return col.replace("[", "[1;");
-9
+
 def italic(col):
 	return col.replace("[", "[3;");
 
@@ -32,14 +33,3 @@ def invert(col):
 
 def customattr(col, attrn):
 	return  col.replace("[", "["+str(attrn)+";");
-	
-	def test_plg():
-		for r in range(8):
-			for c in range(8):
-				ctrl_vals=str(30+c)+";"+str(40+r)
-				vals=str(c)+str(r)
-				print (ctrl_pfx+ctrl_valss+ctrl_sfx+vals+reset)
-	
-	if __name=="__main__":
-		print ("Test colors table")
-		test_pkg();
--- py/shcolar/test.py
+++ py/shcolar/test.py
@@ -1,12 +1,16 @@
 #! /usr/bin/env python
 import shcolar
 def test_pkg(attr_n):
-	print ("Table with additional attribute", str(attr_n))
-	for r in range(8):
-		for c in range(8):
+	print ("Test table with additional attribute", str(attr_n))
+	for r in range(-1, shcolar.colors_n):
+		if r==-1:
+			print(shcolar.bg.white+shcolar.fg.black)
+		for c in range(shcolar.colors_n):
 			ctrl_vals=str(attr_n)+";"+str(30+c)+";"+str(40+r)
-			vals=" "+str(c)+str(r)+" "
-			print (shcolar.ctrl_pfx+ctrl_vals+shcolar.ctrl_sfx+vals+shcolar.reset, end=("" if c!=7 else "\n"))
+			vals=" "+str(c)+("-" if r==-1 else str(r))+" "
+			print (shcolar.ctrl_pfx+ctrl_vals+shcolar.ctrl_sfx+vals+shcolar.reset, end=("" if c<7 else "\n"))
+		if r==-1:
+			print("FG"+shcolar.reset)
 
 if __name__=="__main__":
 	print ("Test Colors tables")
