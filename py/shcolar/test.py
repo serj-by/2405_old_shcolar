02143cdbba00a07b1716a5c9b9876fd62f56388b
Serj Phone <serj-by@users.noreply.github.com>
2024-04-07 09:43:37 +0300

    test attrs

--- py/shcolar/test.py
+++ py/shcolar/test.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python
 import shcolar
-def test_pkg():
-                for r in range(8):
-                        for c in range(8):
-                                ctrl_vals=str(30+c)+";"+str(40+r)
-                                vals=" "+str(c)+str(r)+" "
-                                print (shcolar.ctrl_pfx+ctrl_vals+shcolar.ctrl_sfx+vals+shcolar.reset, end=("" if c!=7 else "\n"))
+def test_pkg(attr_n):
+	print ("Table with additional attribute", str(attr_n))
+	for r in range(8):
+		for c in range(8):
+			ctrl_vals=str(attr_n)+";"+str(30+c)+";"+str(40+r)
+			vals=" "+str(c)+str(r)+" "
+			print (shcolar.ctrl_pfx+ctrl_vals+shcolar.ctrl_sfx+vals+shcolar.reset, end=("" if c!=7 else "\n"))
 
 if __name__=="__main__":
-	print ("Test colors table")
-	test_pkg();
+	print ("Test Colors tables")
+	for at in range(10):
+		test_pkg(at);
