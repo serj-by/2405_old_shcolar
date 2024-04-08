6b1c2a62eccde5e73cb2cd7677dec00529925798
Serj Phone <serj-by@users.noreply.github.com>
2024-04-08 23:28:43 +0300

    with docpy comments in modules

--- py/shcolar/bg.py
+++ py/shcolar/bg.py
@@ -1,16 +1,35 @@
+
+reset="\033[49m";
+
 black="\033[40m";
+"""Escape sequence for background black color"""
 red="\033[41m";
+"""Escape sequence for background red color"""
 green="\033[42m";
+"""Escape sequence for background green color"""
 yellow="\033[43m";
+"""Escape sequence for background yellow color"""
 blue="\033[44m";
+"""Escape sequence for background blue color"""
 magenta="\033[45m";
+"""Escape sequence for background magenta color"""
 cyan="\033[46m";
+"""Escape sequence for background cyan color"""
 white="\033[47m";
+"""Escape sequence for background white color"""
 gray="\033[100m";
+"""Escape sequence for background gray color"""
 brightRed="\033[101m";
+"""Escape sequence for background bright red color"""
 brightGreen="\033[102m";
+"""Escape sequence for background bright green color"""
 brightYellow="\033[103m";
+"""Escape sequence for background bright yellow color"""
 brightBlue="\033[104m";
+"""Escape sequence for background bright blue color"""
 brightMagenta="\033[105m";
+"""Escape sequence for background bright magenta color"""
 brightCyan="\033[106m";
+"""Escape sequence for background bright cyan color"""
 brightWhite="\033[107m";
+"""Escape sequence for background bright white color"""
--- py/shcolar/fg.py
+++ py/shcolar/fg.py
@@ -1,16 +1,35 @@
+
+reset="\033[39m";
+
 black="\033[30m";
+"""Escape sequence for foreground black color"""
 red="\033[31m";
+"""Escape sequence for foreground red color"""
 green="\033[32m";
+"""Escape sequence for foreground green color"""
 yellow="\033[33m";
+"""Escape sequence for foreground yellow color"""
 blue="\033[34m";
+"""Escape sequence for foreground blue color"""
 magenta="\033[35m";
+"""Escape sequence for foreground magenta color"""
 cyan="\033[36m";
+"""Escape sequence for foreground cyan color"""
 white="\033[37m";
+"""Escape sequence for foreground white color"""
 gray="\033[90m";
+"""Escape sequence for foreground gray color"""
 brightRed="\033[91m";
+"""Escape sequence for foreground bright red color"""
 brightGreen="\033[92m";
+"""Escape sequence for foreground bright green color"""
 brightYellow="\033[93m";
+"""Escape sequence for foreground bright yellow color"""
 brightBlue="\033[94m";
+"""Escape sequence for foreground bright blue color"""
 brightMagenta="\033[95m";
+"""Escape sequence for foreground bright magenta color"""
 brightCyan="\033[96m";
+"""Escape sequence for foreground bright cyan color"""
 brightWhite="\033[97m";
+"""Escape sequence for foreground bright white color"""
